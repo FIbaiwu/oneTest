6ca0147c193ea7874f60ba41c30775a4aec3f33b
bai <bai@163.com>
2022-01-09 13:19:52 +0800

    第四次提交

--- SSM/ssm-user/src/main/java/com/bjpowernode/controller/UserController.java
+++ SSM/ssm-user/src/main/java/com/bjpowernode/controller/UserController.java
@@ -1,26 +1,27 @@
 package com.bjpowernode.controller;
-
 import com.bjpowernode.domain.SsmUsers;
 import com.bjpowernode.service.SsmUsersService;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-
 import javax.annotation.Resource;
 import java.util.List;
-
-@RestController
+@CrossOrigin
 @RequestMapping("/user")
+@RestController
 public class UserController {
     private static final Integer PAGE_BROWING = 5;
     @Resource
     private SsmUsersService service;
+
     @RequestMapping("/selectUserPage")
     public List<SsmUsers> selectUserPage(String userName,String userSex, Integer page){
         int startpage = 0;
         List<SsmUsers> list = null;
         if(page ==null){
             list = service.findUserPage(userName, userSex, startpage);
+            return list;
         }
 
         startpage = (page -1)* PAGE_BROWING;
@@ -28,14 +29,16 @@ public class UserController {
         return list;
     }
     @RequestMapping("/getRowCount")
-        public Integer getRowCount(String userName,String userSex){
+    public Integer getRowCount(String userName,String userSex){
         Integer count = service.findRowCount(userName, userSex);
         return count;
     }
-//    //user/ deleteUserById?userId= 15968162087363060
+
+    //    //user/ deleteUserById?userId= 15968162087363060
     @RequestMapping("/deleteUserById")
     public Integer deleteUserById(String userId){
         Integer result = service.removeUserById(userId);
         return result;
     }
 }
+
